9cfa671f669b5938885a06a6775e87f8c394241a
Leslie G <lesliegonzalez@me.com>
2020-07-03 12:26:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,11 @@
 def oxford_comma(array)
   [array].join(" , ")
+  if (array.join).length == 2
+    (array.join).join(" and ")
+  elsif (array.join).length >= 3
+    array.split << [(", and ")-2]
+ 
+ 
+ 
  
 end
