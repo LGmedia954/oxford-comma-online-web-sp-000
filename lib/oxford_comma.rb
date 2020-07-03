c03621043ae87b62e368b255f01ad69a6a50bd40
Leslie G <lesliegonzalez@me.com>
2020-07-03 12:29:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   [array].join(" , ")
-  if array.length == 2
+  if array.length == array.length2
     array.join(" and ")
   elsif array.length >= 3
     array.split << [(", and ")-2]
